929408c098fb65cbb52d40a9b0fa4f60de65f094
Arsya <arsyafikrisabilillah10@gmail.com>
2025-09-30 19:17:14 +0700

    kalkulator

---
+++ source/dataStructure/Calculator.java
@@ -0,0 +1,7 @@
+package source.dataStructure;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ source/dataStructure/CalculatorTest.java
@@ -0,0 +1,20 @@
+package source.dataStructure;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        Calculator calc = new Calculator();
+        int codeInput1 = 2;
+        int codeInput2 = 3;
+        int codeOutput = calc.add(codeInput1, codeInput2);
+        int expectedOutput = 5;
+        try {
+            assertEquals("Addition test failed:", expectedOutput, codeOutput);
+        } catch (AssertionError ae) {
+            System.out.println("ae");
+        }
+    }
+}
